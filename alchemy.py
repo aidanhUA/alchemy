36bd012bd894384b5128ba41e563c03cc1691771
Aidan Hickman <aidan.hickman@urbanairship.com>
2021-01-19 18:09:42 +0000

    Adding call to get Access Token from Spotify, then retrieve users top artists.

--- alchemy.py
+++ alchemy.py
@@ -1,10 +1,10 @@
 from flask import Flask, request
-from os import environ, path
+from os import path
 from dotenv import load_dotenv
+import requests
+import utilities
 
 app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG')
-SPOTIFY_TOKEN_API = 'https://accounts.spotify.com/api/token'
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
@@ -17,17 +17,27 @@ def hello_world():
 @app.route('/spotify-client-id', methods=['POST'])
 def get_client_id():
     app.logger.info('Retrieved Spotify Client ID')
-    return {'spotify_client_id': environ.get('SPOTIFY_CLIENT_ID')}
+    return utilities.get_spotify_client_id()
 
 
-@app.route('/spotify-callback', methods=['GET'])
-def handle_spotify_callback():
+@app.route('/spotify-login-callback', methods=['GET'])
+def handle_spotify_login_callback():
     try:
         code = request.args.get('code')
-        print(code)
-
-        # Return a message to go back to the site.
-        return 'Hello'
+        app.logger.info('Received Spotify Auth Code')
     except ValueError:
         app.logger.info('Spotify login unsuccessful')
 
+    try:
+        token_payload = utilities.get_spotify_access_tokens(code)
+    except requests.HTTPError as e:
+        return 'That did not work as planned. Try it again maybe?'
+
+    try:
+        users_top_read = utilities.get_users_top_read(token_payload['access_token'])
+    except requests.HTTPError as e:
+        return 'For some reason I can not get your top artists from Spotify. ' \
+               'This usually happens if you listen to a lot of Celine Dion'
+
+    ### HERE WE COMPARE TO MY DATA ###
+    return 'Thanks, we got it!'
